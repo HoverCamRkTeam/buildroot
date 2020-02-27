7e68adea50d5999a06e13eb77bcabf9bbb1dc631
Bruce Barnes <bruce@veratekdesign.com>
2020-02-27 13:37:24 -0800

    Added configuration for HID3 board and hid3mtouch package
    Change-Id: I94a4fb42bafc6596102ada3ad7c853fe616d86a8

---
+++ package/hovercam/hid3mtouch/hid3mtouch.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# hid3mtouch
+#
+################################################################################
+
+HID3MTOUCH_VERSION = 1.0
+HID3MTOUCH_SITE = $(TOPDIR)/../app/SiBeamHID3/hid3mtouch
+HID3MTOUCH_SITE_METHOD = local
+
+define HID3MTOUCH_BUILD_CMDS
+	cd $(@D); $(TARGET_CC) -pthread -o hid3mtouch *.c
+endef
+
+define HID3MTOUCH_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/hid3mtouch $(TARGET_DIR)/usr/bin/hid3mtouch
+	$(INSTALL) -D -m 0755 $(@D)/S90hid3mtouch $(TARGET_DIR)/etc/init.d/S90hid3mtouch
+endef
+
+$(eval $(generic-package))
+
