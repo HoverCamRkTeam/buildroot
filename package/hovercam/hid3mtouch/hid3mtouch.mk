54638996049844521e448ba705362f344b0e3675
timothy barnes <timothy@veratekdesign.com>
2020-03-06 17:57:17 -0800

    Modified hid3mtouch.mk to install firmware update script
    Change-Id: I26f8bb666fb210ff64965394a05e97b0b6a595e9

--- package/hovercam/hid3mtouch/hid3mtouch.mk
+++ package/hovercam/hid3mtouch/hid3mtouch.mk
@@ -15,6 +15,7 @@ endef
 define HID3MTOUCH_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/hid3mtouch $(TARGET_DIR)/usr/bin/hid3mtouch
 	$(INSTALL) -D -m 0755 $(@D)/S90hid3mtouch $(TARGET_DIR)/etc/init.d/S90hid3mtouch
+	$(INSTALL) -D -m 0755 $(@D)/upgrade_firmware $(TARGET_DIR)/upgrade_firmware
 endef
 
 $(eval $(generic-package))
